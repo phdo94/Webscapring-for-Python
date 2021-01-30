812ad3552631c1ac7355d2028a15cb6ae2ef08cb
ParkHeondo <gjseh94@gmail.com>
2021-01-30 16:43:11 +0900

    User-agent and get nodocoding html

---
+++ webscraping_basic/5_user_agent.py
@@ -0,0 +1,7 @@
+import requests
+url = "http://nadocoding.tistory.com"
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"}
+res = requests.get(url, headers=headers)
+res.raise_for_status()
+with open("nadocoding.html", 'w', encoding="utf-8") as f:
+    f.write(res.text)
