04203d26a211bfd50489a2231046d914ec438a7c
ParkHeondo <gjseh94@gmail.com>
2021-02-17 17:30:00 +0900

    Use headless Chrome and change user-agent

---
+++ webscraping_basic/18_headless_chrome_useragent.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.headless = True
+options.add_argument("window-size=2560x1600")
+
+browser = webdriver.Chrome("./chromedriver", options=options)
+browser.maximize_window()
+
+url = "https://www.whatismybrowser.com/detect/what-is-my-user-agent"
+browser.get(url)
+
+# Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) 
+# AppleWebKit/537.36 (KHTML, like Gecko) 
+# Chrome/88.0.4324.182 Safari/537.36
+detected_value = browser.find_element_by_id("detected_value")
+print(detected_value.text)
+browser.quit()
