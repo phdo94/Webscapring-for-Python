ea1d5c740a218e008b343fe73b1ad5a3c0bf05b6
ParkHeondo <gjseh94@gmail.com>
2021-02-23 15:04:04 +0900

    Make project file and practice scraping

--- webscraping_basic/18_headless_chrome_useragent.py
+++ webscraping_basic/18_headless_chrome_useragent.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 options = webdriver.ChromeOptions()
 options.headless = True
 options.add_argument("window-size=2560x1600")
+options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36")
 
 browser = webdriver.Chrome("./chromedriver", options=options)
 browser.maximize_window()
